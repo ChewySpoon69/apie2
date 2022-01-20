17b6d5c370e33c650cf9e35fcf66697192581d0f
ChewySpoon69 <97991995+ChewySpoon69@users.noreply.github.com>
2022-01-20 15:27:15 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# THIS PROJECT IS DEPRECATED AND NOT MAINTAINED
+
+This project has many issues as it was written when I was far less experienced and still learning Go
+It is meant for use with expression2 chips for the game garrysmod
+
+If you want a better api for random foxes check out https://github.com/plally/fox.pics-backend
---
+++ random.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+)
+
+var foxFiles []string
+
+func foxInit() {
+	rand.Seed(time.Now().Unix())
+	files, err := ioutil.ReadDir("fox/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, f := range files {
+		foxFiles = append(foxFiles, f.Name())
+	}
+
+}
+func randomFoxHandler(w http.ResponseWriter, r *http.Request) {
+	path := foxFiles[rand.Intn(len(foxFiles))]
+	data, err := ioutil.ReadFile("fox/" + path)
+	if err == nil {
+		fmt.Println()
+		w.Write(data)
+	}
+	w.Header().Add("Content-Type", getContentType(path))
+
+}
+
+func randomFoxHandlerJson(w http.ResponseWriter, r *http.Request) {
+	path := foxFiles[rand.Intn(len(foxFiles))]
+	data, err := ioutil.ReadFile("fox/" + path)
+	if err == nil {
+		fmt.Println()
+		w.Write(data)
+	}
+	w.Header().Add("Content-Type", getContentType(path))
+
+}
+func getContentType(p string) string {
+	if strings.HasSuffix(p, ".jpg") {
+		return "image/jpeg"
+	} else if strings.HasSuffix(p, ".png") {
+
+		return "image/png"
+	}
+	return ""
+}
